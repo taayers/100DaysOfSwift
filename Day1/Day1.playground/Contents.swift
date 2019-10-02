b5c733257b87e09de8ec6504eba9c82793b1a7de
Todd Ayers <taayers712@gmail.com>
2019-10-02 13:18:36 -0400

    Day 1 commit

---
+++ Day1/Day1.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Goodbye"
+
+var age = 38
+var population = 8_000_000
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141
+var awesome = true
+
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+let taylor = "swift"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
