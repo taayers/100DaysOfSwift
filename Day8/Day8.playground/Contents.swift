635cc0b727b945cb58c66a5466e46b017f83dc88
Todd Ayers <taayers712@gmail.com>
2019-10-08 10:22:43 -0400

    Day 8 commit

---
+++ Day8/Day8.playground/Contents.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+var str = "Hello, playground"
+
+struct Sport {
+    var name: String
+    var isOlympicSport: Bool
+    
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+var tennis = Sport(name: "Tennis", isOlympicSport: true)
+print(tennis.name)
+
+tennis.name = "Lawn Tennis"
+
+let chessBoxing = Sport(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+struct City {
+    var population: Int
+    
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+let london = City(population: 9_000_000)
+london.collectTaxes()
+
+struct Person {
+    var name: String
+    
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+
+var person = Person(name: "Ed")
+person.makeAnonymous()
+print(person.name)
+
+let string = "Do or do not. There is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+var toys = ["Woody"]
+toys.count
+toys.append("Buzz")
+toys.firstIndex(of: "Buzz")
+toys.sorted()
+toys.remove(at: 0)
