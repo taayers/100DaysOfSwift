939fc253179d05f331db1646a60c09be85e696ea
Todd Ayers <taayers712@gmail.com>
2019-10-15 17:20:15 -0400

    Day 15 commit

---
+++ Day15/Day15.playground/Contents.swift
@@ -0,0 +1,115 @@
+import UIKit
+
+var str = "Hello, playground"
+
+struct Person {
+    var clothes: String
+    var shoes: String
+
+    func describe() {
+        print("I like wearing \(clothes) with \(shoes)")
+    }
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+taylor.describe()
+other.describe()
+
+struct Dude {
+    var clothes: String {
+        willSet {
+            updateUI(msg: "I'm changing from \(clothes) to \(newValue)")
+        }
+
+        didSet {
+            updateUI(msg: "I just changed from \(oldValue) to \(clothes)")
+        }
+    }
+}
+
+func updateUI(msg: String) {
+    print(msg)
+}
+
+var ed = Dude(clothes: "T-shirts")
+ed.clothes = "short skirts"
+
+struct Girl {
+    var age: Int
+
+    var ageInDogYears: Int {
+        get {
+            return age * 7
+        }
+    }
+}
+
+var fan = Girl(age: 25)
+print(fan.ageInDogYears)
+
+struct TaylorFan {
+    static var favoriteSong = "Look What You Made Me Do"
+
+    var name: String
+    var age: Int
+    var id: String
+}
+
+let fangirl = TaylorFan(name: "Jesse", age: 25, id: "11111")
+print(TaylorFan.favoriteSong)
+
+class Album {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+    
+    func getPerformance() -> String {
+        return "The album \(name) sold lots"
+    }
+}
+
+class StudioAlbum: Album {
+    var studio: String
+
+    init(name: String, studio: String) {
+        self.studio = studio
+        super.init(name: name)
+    }
+    
+    override func getPerformance() -> String {
+        return "The studio album \(name) sold lots"
+    }
+}
+
+class LiveAlbum: Album {
+    var location: String
+
+    init(name: String, location: String) {
+        self.location = location
+        super.init(name: name)
+    }
+    
+    override func getPerformance() -> String {
+        return "The live album \(name) sold lots"
+    }
+}
+
+var taylorSwift = StudioAlbum(name: "Taylor Swift", studio: "The Castles Studios")
+var fearless = StudioAlbum(name: "Speak Now", studio: "Aimeeland Studio")
+var iTunesLive = LiveAlbum(name: "iTunes Live from SoHo", location: "New York")
+
+var allAlbums: [Album] = [taylorSwift, fearless, iTunesLive]
+
+for album in allAlbums {
+    print(album.getPerformance())
+    
+    if let studioAlbum = album as? StudioAlbum {
+        print(studioAlbum.studio)
+    } else if let liveAlbum = album as? LiveAlbum {
+        print(liveAlbum.location)
+    }
+}
+
